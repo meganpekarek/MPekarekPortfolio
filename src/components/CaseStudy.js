5df37a4f9f2dfc857861c20f8aa4343321ce9790
Megan Pekarek <mpekarek9@gmail.com>
2021-01-29 10:46:35 -0600

    responsive case study component

--- src/components/CaseStudy.js
+++ src/components/CaseStudy.js
@@ -10,12 +10,20 @@ class CaseStudy extends React.Component {
 
   render() {
     return (
-        <div className={this.props.alignRight ? "home__caseStudy home__rightAlignedCaseStudy" : "home__caseStudy home__leftAlignedCaseStudy"}>
-            <img className="home_caseStudyPhoto" src={this.props.coverPhoto}/>
-            <div className="home__caseStudyDesc">
-                {this.props.title}
+        <React.Fragment>
+            <div className={this.props.alignRight ? "home__caseStudy home__rightAlignedCaseStudy" : "home__caseStudy home__leftAlignedCaseStudy"}>
+                <img className="home_caseStudyPhoto" src={this.props.coverPhoto}/>
+                <div className="home__caseStudyDesc">
+                    {this.props.title}
+                </div>
             </div>
-        </div>
+            <div className="home__mobileCaseStudy">
+                <img className="home_mobileCaseStudyPhoto" src={this.props.coverPhoto}/>
+                <div className="home__mobileCaseStudyDesc">
+                    {this.props.title}
+                </div>
+            </div>
+        </React.Fragment>
     );
   }
 }
