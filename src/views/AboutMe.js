ee7b755cf849e48fceefded4d6308b39c9c0edd6
Megan Pekarek <mpekarek9@gmail.com>
2021-02-08 15:19:52 -0600

    about me updates

--- src/views/AboutMe.js
+++ src/views/AboutMe.js
@@ -1,22 +1,34 @@
-import '../App.css';
-import React from 'react'
-import SkillsChart from '../images/SkillsChart.png'
-import SkillsGraph from '../components/SkillsGraph'
+import "../App.css";
+import React from "react";
+import SkillsChart from "../images/SkillsChart.png";
+import SkillsGraph from "../components/SkillsGraph";
 
 class AboutMe extends React.Component {
-
   render() {
-
     return (
-        <React.Fragment>
-            <section className="home__skillsWrapper">
-                <h3>My Skills</h3>
-                <div className="home__skillsChartWrapper">
-                    <img className="home__skillsChart" src={SkillsChart} alt="skills-chart" />
-                </div>
-                <SkillsGraph />
-            </section>
-        </React.Fragment>
+      <React.Fragment>
+        <section className="aboutMe__headerSection">
+          <div>
+            <div className="aboutMe__infoWrapper">
+              <h3>Why I Love Design</h3>
+              <span className="aboutMe__infoContent">Psychology has always been an interest to me, so UX design has been the perfect crossover between my love for software and desire to learn about the way people function.</span>
+            </div>
+            <div className="aboutMe__infoWrapper">
+              <h3>Why I Love Development</h3>
+              <span className="aboutMe__infoContent">Problem solving is what I live for. I love being a part of answering the “how do we bring these designs to life?” question. </span>
+            </div>
+          </div>
+          <img
+            className="aboutMe__skillsChart"
+            src={SkillsChart}
+            alt="skills-chart"
+          />
+        </section>
+        <section className="aboutMe__skillsWrapper">
+          <h3>My Skills</h3>
+          <SkillsGraph />
+        </section>
+      </React.Fragment>
     );
   }
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,45 +1,62 @@
-import '../App.css';
-import React from 'react'
-import NSACoverPhoto from '../images/NSACoverPhoto.png'
-import SheCanicCoverPhoto from '../images/SheCanicCoverPhoto.png'
-import CGCoverPhoto from '../images/CGCoverPhoto.png'
-import CaseStudy from '../components/CaseStudy'
+import "../App.css";
+import React from "react";
+import NSACoverPhoto from "../images/NSACoverPhoto.png";
+import SheCanicCoverPhoto from "../images/SheCanicCoverPhoto.png";
+import CGCoverPhoto from "../images/CGCoverPhoto.png";
+import CaseStudy from "../components/CaseStudy";
+// import { Jumbotron, Button, Nav } from "react-bootstrap";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Home extends React.Component {
-
   render() {
-
     return (
-        <React.Fragment>
-            <section className="home__headerContent">
-                <div className="home__titleSection">
-                    <h1 className="home__header">Megan Pekarek</h1>
-                    <h3 className="home__subheader">Front End Developer and UX/UI Designer</h3>
-                </div>
-                <div className="home__infoSection">
-                    <div className="home__careerInfo">
-                        <h3>&lt; Developer /&gt;</h3>
-                        <div className="home__headerDesc">
-                        My journey into the world of software started with my degree in Computer Science. I quickly realized that front end development is where it’s at. Bringing beautiful designs to life never gets old.
-                        </div>
-                    </div>
-                    <div className="home__careerInfo">
-                        <h3>Designer</h3>
-                        <div className="home__headerDesc">
-                        I love making pretty things, but what I am most passionate about is user research and testing. I want to know what my users want and why they want it. 
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="home__uxWorkWrapper">
-                <h3>My UX/UI Work</h3>
-                <div className="home__uxWorkCaseStudies">
-                    <CaseStudy title="Common Ground Meditation Center Website Redesign Prototype" coverPhoto={CGCoverPhoto} />
-                    <CaseStudy title="SheCanic Vehicle Maintenance Mobile App Prototype" alignRight={true} coverPhoto={SheCanicCoverPhoto} />
-                    <CaseStudy title="NSA Website Redesign Prototype" coverPhoto={NSACoverPhoto} />
-                </div>
-            </section>
-        </React.Fragment>
+      <React.Fragment>
+        <section className="home__headerContent">
+          <div className="home__titleSection">
+            <h1 className="home__header">Megan Pekarek</h1>
+            <h3 className="home__subheader">
+              Front End Developer and UX/UI Designer
+            </h3>
+          </div>
+          <div className="home__infoSection">
+            <div className="home__careerInfo">
+              <h3>&lt; Developer /&gt;</h3>
+              <div className="home__headerDesc">
+                My journey into the world of software started with my degree in
+                Computer Science. I quickly realized that front end development
+                is where it’s at. Bringing beautiful designs to life never gets
+                old.
+              </div>
+            </div>
+            <div className="home__careerInfo">
+              <h3>Designer</h3>
+              <div className="home__headerDesc">
+                I love making pretty things, but what I am most passionate about
+                is user research and testing. I want to know what my users want
+                and why they want it.
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="home__uxWorkWrapper">
+          <h3>My UX/UI Work</h3>
+          <div className="home__uxWorkCaseStudies">
+            <CaseStudy
+              title="Common Ground Meditation Center Website Redesign Prototype"
+              coverPhoto={CGCoverPhoto}
+            />
+            <CaseStudy
+              title="SheCanic Vehicle Maintenance Mobile App Prototype"
+              alignRight={true}
+              coverPhoto={SheCanicCoverPhoto}
+            />
+            <CaseStudy
+              title="NSA Website Redesign Prototype"
+              coverPhoto={NSACoverPhoto}
+            />
+          </div>
+        </section>
+      </React.Fragment>
     );
   }
 }
