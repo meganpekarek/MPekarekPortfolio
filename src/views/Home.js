015e156164ee7e0a7261d0b00f7c627cbdcd90e5
Megan Pekarek <mpekarek9@gmail.com>
2021-02-09 10:00:15 -0600

    cleaner home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -14,27 +14,14 @@ class Home extends React.Component {
         <section className="home__headerContent">
           <div className="home__titleSection">
             <h1 className="home__header">Megan Pekarek</h1>
-            <h3 className="home__subheader">
-              Front End Developer and UX/UI Designer
-            </h3>
-          </div>
-          <div className="home__infoSection">
-            <div className="home__careerInfo">
-              <h3>&lt; Developer /&gt;</h3>
-              <div className="home__headerDesc">
-                My journey into the world of software started with my degree in
-                Computer Science. I quickly realized that front end development
-                is where it’s at. Bringing beautiful designs to life never gets
-                old.
-              </div>
-            </div>
-            <div className="home__careerInfo">
-              <h3>Designer</h3>
-              <div className="home__headerDesc">
-                I love making pretty things, but what I am most passionate about
-                is user research and testing. I want to know what my users want
-                and why they want it.
-              </div>
+            <div className="home__subheaderWrapper">
+                <h3 className="home__subheader home__leftSubheader">
+                Developer
+                </h3>
+                <div className="home__subheaderSeparator"></div>
+                <h3 className="home__subheader home__rightSubheader">
+                Designer
+                </h3>
             </div>
           </div>
         </section>
